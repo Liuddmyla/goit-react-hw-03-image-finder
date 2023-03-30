967c88701b88cae47fa2bfc3669c00f21725992f
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-03-30 10:54:11 +0300

    button

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,9 @@
+// import PropTypes from 'prop-types';
+
+const Bytton = ({onClick}) => {
+    return (
+        <button type="button" className="button" onClick={onClick}>Load more</button>
+    )
+}
+
+export default Bytton;
--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -3,6 +3,7 @@ import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from '../imageGalleryItem/ImageGalleryItem';
 import { Loader } from '../loader/Loader';
+import  Button  from '../button/Button';
 
 export default class ImageGallery extends Component {
     state = {
@@ -17,7 +18,7 @@ export default class ImageGallery extends Component {
         const nextName = this.props.imageName;
         const URL = `https://pixabay.com/api/?q=${nextName}&page=${this.state.page}&key=33641920-b059883ebd7147c979fd953b4&image_type=photo&orientation=horizontal&per_page=12`;
 
-        if (prevName !== nextName) {
+        if (prevName !== nextName || prevState.page !== this.state.page) {
             this.setState({ status: 'pending' });
 
             fetch(URL).then(response => {
@@ -30,14 +31,16 @@ export default class ImageGallery extends Component {
                     if (hits.length === 0) {
                         toast.error('Sorry, there are no images matching your search query. Please try again.', {autoClose: 2000,});
                     }
-                    this.setState({ images: hits, status: 'resolved' })
+                    this.setState(prevState => {
+                        return { images:[...prevState.images, ...hits ], status: 'resolved' }
+                    })
                 } )
                 .catch(error => this.setState({ error, status: 'rejected' }));
         }       
     }
 
-    componentWillUnmount() {
-        
+    handleClick = () => {
+        this.setState({ page: this.state.page + 1 });
     }
 
     render() {
@@ -55,7 +58,8 @@ export default class ImageGallery extends Component {
 
         if (this.state.status === 'resolved' && this.state.images.length > 0) {
             return <ul className="gallery">
-                <ImageGalleryItem images={this.state.images}/>
+                <ImageGalleryItem images={this.state.images} />
+                <Button onClick={ this.handleClick} />
             </ul>
         } 
     }
